53fed4b7311b7d2ce1c5815ed4f2b37bfbdb457d
Nicolas Roduit <nicolas.roduit@gmail.com>
2017-09-08 19:04:59 +0200

    Add example for C-Move at series or image level

--- src/test/java/org/weasis/dicom/CMoveNetTest.java
+++ src/test/java/org/weasis/dicom/CMoveNetTest.java
@@ -11,11 +11,13 @@
 package org.weasis.dicom;
 
 import org.dcm4che3.data.Tag;
+import org.dcm4che3.net.QueryOption;
 import org.dcm4che3.net.Status;
 import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
 import org.junit.Test;
 import org.weasis.dicom.op.CMove;
+import org.weasis.dicom.param.AdvancedParams;
 import org.weasis.dicom.param.DicomNode;
 import org.weasis.dicom.param.DicomParam;
 import org.weasis.dicom.param.DicomProgress;
@@ -41,11 +43,20 @@ public class CMoveNetTest {
         /**
          * The following parameters must be changed to get a successful test.
          */
-        DicomParam[] params =
-            { new DicomParam(Tag.StudyInstanceUID, "1.2.826.0.1.3680043.9.4113.1.2.1754115794.5304.1404814421.494") };
+
+        // Move study
+        DicomParam[] params = { new DicomParam(Tag.StudyInstanceUID, "1.3.46.670589.16.5.100.20091127134147.64690") };
+        // // Move series
+        // DicomParam[] params = { new DicomParam(Tag.QueryRetrieveLevel, "SERIES"),
+        // new DicomParam(Tag.SeriesInstanceUID, "2.25.62689877621998739235261278936628920157") };
+        // // Move image
+        // DicomParam[] params = { new DicomParam(Tag.QueryRetrieveLevel, "IMAGE"),
+        // new DicomParam(Tag.SOPInstanceUID, "1.2.840.113543.6.6.3.4.637463244096141531813342472862196132286") };
         DicomNode calling = new DicomNode("WEASIS-SCU");
-        DicomNode called = new DicomNode("DICOMSERVER", "dicomserver.co.uk", 11112);
-        DicomState state = CMove.process(calling, called, "AET-DESTINATION", progress, params);
+        DicomNode called = new DicomNode("DCM4CHEE", "localhost", 11112);
+        AdvancedParams options = new AdvancedParams();
+        options.getQueryOptions().add(QueryOption.RELATIONAL); // Required for QueryRetrieveLevel other than study
+        DicomState state = CMove.process(options, calling, called, "WEASIS-SCU", progress, params);
 
         // Should never happen
         Assert.assertNotNull(state);
@@ -60,7 +71,7 @@ public class CMoveNetTest {
         // see org.dcm4che3.net.Status
         // See server log at http://dicomserver.co.uk/logs/
         Assert.assertThat(state.getMessage(), state.getStatus(), IsEqual.equalTo(Status.Success));
-        Assert.assertFalse("No DICOM RSP Object", state.getDicomRSP().isEmpty());
+        // Assert.assertFalse("No DICOM RSP Object", state.getDicomRSP().isEmpty());
     }
 
 }
